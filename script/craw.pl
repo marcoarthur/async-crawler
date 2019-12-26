b81e8702739ff4e70109f20cdc69dcd572f3ed07
Marco Arthur <arthurpbs@gmail.com>
2019-12-26 00:05:00 -0300

    Minor change to script to accept site

--- script/craw.pl
+++ script/craw.pl
@@ -4,8 +4,10 @@ use warnings;
 use lib qw(./lib);
 use Async::Crawler;
 
+my $site = $ARGV[0] || 'https://www.folha.uol.com.br/fsp';
+
 my $ac = Async::Crawler->new(
-    targets => [ 'https://www.folha.uol.com.br/fsp' ],
+    targets => [ $site ],
     elements => [
         {
             selector => '.c-news__body > p',
